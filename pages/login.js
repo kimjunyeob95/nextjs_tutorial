050f5c55ee76d005d20546beaba23e82700644e2
kimjunyeob <kimjun@hanbom.com>
2022-01-27 18:55:16 +0900

    로그인 처리 업데이트

--- pages/login.js
+++ pages/login.js
@@ -37,14 +37,12 @@ export default function Login() {
         const expires = new Date();
         //1*60*1000 => 1분
         expires.setTime(expires.getTime() + 2 * 60 * 60 * 1000); //2시간
-        if (!cookies.mInfo) {
-          setCookie("mInfo", res.data.mInfo, {
-            path: "/",
-            secure: true,
-            sameSite: "none",
-            expires,
-          });
-        }
+        setCookie("mInfo", res.data.mInfo, {
+          path: "/",
+          secure: true,
+          sameSite: "none",
+          expires,
+        });
         dispatch({ type: "login", mInfo: res.data.mInfo });
         if (router.query.returnUrl) {
           router.push(router.query.returnUrl).then(() => alert(res.data.msg));
